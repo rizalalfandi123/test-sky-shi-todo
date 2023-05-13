546a557394100bbdca14809015b66ea315663861
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-05-13 22:04:25 +0700

    remove suspense fallback

--- src/components/page-wrapper/page-wrapper.tsx
+++ src/components/page-wrapper/page-wrapper.tsx
@@ -1,17 +1,17 @@
 import type { PropsWithChildren } from "react";
 
 import { Suspense } from "react";
-import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
+// import Backdrop from "@mui/material/Backdrop";
+// import CircularProgress from "@mui/material/CircularProgress";
 
 export const PageWrapper = ({ children }: PropsWithChildren) => {
   return (
     <Suspense
-      fallback={
-        <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      }
+      // fallback={
+      //   <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open>
+      //     <CircularProgress color="inherit" />
+      //   </Backdrop>
+      // }
     >
       {children}
     </Suspense>
