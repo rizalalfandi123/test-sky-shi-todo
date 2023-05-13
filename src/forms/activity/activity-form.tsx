28f6acee460d86ab6cabbd5e09f751329a6aec39
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-05-13 19:50:14 +0700

    update cy dataset

--- src/forms/activity/activity-form.tsx
+++ src/forms/activity/activity-form.tsx
@@ -6,7 +6,6 @@ import MenuItem from "@mui/material/MenuItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Select from "@mui/material/Select";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Label } from "./activity-form.styled";
 import { Dispatch } from "react";
 import { SetStateAction } from "react";
